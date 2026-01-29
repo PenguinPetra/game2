ba038464bc6dca0453d99cefe61c51dae0e15a21
PenguinPetra <167444423+PenguinPetra@users.noreply.github.com>
2026-01-29 23:10:45 +0900

    Add files via upload

--- bgm.js
+++ bgm.js
@@ -1,6 +1,6 @@
 // bgm.js
 (function() {
-    const bgmPath = 'oyubgm_107_happy_lemonade_master.mp3'; 
+    const bgmPath = 'bgm.mp3'; 
     const bgm = new Audio(bgmPath);
 
     bgm.loop = true;      // ループ再生（途中で終わったら再度流す）
